3a398d360aa1fe766eed4cb5c00ef69e4ea45f04
Adwaith S Raj <rajadwaith4@gmail.com>
2026-09-26 12:23:33 +0530

    feat: auth and db sync

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -5,5 +5,11 @@ dotenv.config()
 export const ENV = {
     PORT : process.env.PORT,
     NODE_ENV : process.env.NODE_ENV,
-    DB_URL : process.env.DB_URL
+    DB_URL : process.env.DB_URL,
+    CLIENT_URL:process.env.CLIENT_URL,
+    INGEST_EVENT_KEY:process.env.INGEST_EVENT_KEY,
+    INGEST_SIGNING_KEY:process.env.INGEST_SIGNING_KEY,
+    STREAM_API_KEY:process.env.STREAM_API_KEY,
+    STREAM_API_SECRET:process.env.STREAM_API_SECRET,
+   
 }
